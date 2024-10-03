732387fec12359d20ba6003a317ac0c555657e3e
apoorvvikram01 <135138854+apoorvvikram01@users.noreply.github.com>
2024-10-03 13:54:45 +0530

    project setup with mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import express from "express";
+import dotenv from "dotenv";
+import connect from "./db/mongodb.js";
+import authRoute from './routes/auth.route.js'
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
-const port = process.env.PORT 
+const port = process.env.PORT;
 
-  
-    mongoose.connect(process.env.MONGO_URI)
-        .then (()=>{
-            console.log("Connected to database")
-        })
-        .catch ((error)=>{
-            console.log(error)
-        });
-    
+app.use('/api',authRoute)
 
-
-app.listen(port,()=>
-{
-    console.log(`Server is listening on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+  connect();
+});
